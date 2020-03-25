b7f23696b085b7e41e5b0fe71421a4f5dbc3dcf5
charles clark <charles@pop-os.localdomain>
2020-03-24 23:42:39 -0500

    following deployment tutorial and vue routers.  Created initial compoents

---
+++ noshavequarantine/src/components/secure.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Secure</h1>
+        <div>
+            <router-link to="/">Go Back</router-link>
+        </div>
+    </div>
+</template>
--- noshavequarantine/src/router/index.js
+++ noshavequarantine/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/home'
+import Secure from '@/components/secure'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/secure',
+      name: 'Secure',
+      component: Secure
     }
   ]
 })
+
+export default router
