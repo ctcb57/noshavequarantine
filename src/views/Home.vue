b769a7759f7cdc8e564e226f0698c1ea46373ca6
charles clark <charles@pop-os.localdomain>
2020-03-26 23:57:33 -0500

    removed carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
     <HelloWorld msg="Content Coming Soon"/>
-    <PhotoCarousel />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import PhotoCarousel from '@/components/PhotoCarousel.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
-    PhotoCarousel
+    HelloWorld
   }
 }
 </script>
