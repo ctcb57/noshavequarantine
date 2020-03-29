3a0bb74c0655122fbefd07d2ada1347c5651fe42
charles clark <charles@pop-os.localdomain>
2020-03-29 15:58:40 -0500

    Created initial gallery UI and filled with static images

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="gallery">
     <h1 class="text-center" style="text-decoration: underline">The Evolution of a Beard</h1>
+    <br>
+    <b-container fluid class="p-4 bg-light">
+      <b-row>
+        <b-col cols="3">
+          <b-img thumbnail fluid src="https://i.ibb.co/bNqD4BS/March24.jpg" alt="March 24"></b-img>
+          <h5 class="text-center">March 24</h5>
+        </b-col>
+        <b-col cols="3">
+          <b-img thumbnail fluid src="https://i.ibb.co/Y2QXZ6n/March25.jpg" alt="March 25"></b-img>
+          <h5 class="text-center">March 25</h5>
+        </b-col>
+        <b-col cols="3">
+          <b-img thumbnail fluid src="https://i.ibb.co/pdBY4Cb/March-26.jpg" alt="March 26"></b-img>
+          <h5 class="text-center">March 26</h5>
+        </b-col>
+        <b-col cols="3">
+          <b-img thumbnail fluid src="https://i.ibb.co/XJPj0FR/March27.jpg" alt="March 27"></b-img>
+          <h5 class="text-center">March 27</h5>
+        </b-col>
+        <b-col cols="3">
+          <b-img thumbnail fluid src="https://i.ibb.co/Btkz8zD/March28.jpg" alt="March 28"></b-img>
+          <h5 class="text-center">March 28</h5>
+        </b-col>
+        <b-col cols="3">
+          <b-img thumbnail fluid src="https://i.ibb.co/pQMwz4n/March29.jpg" alt="March 29"></b-img>
+          <h5 class="text-center">March 29</h5>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
